8930f712a4b13414b208b9ca76df49a0ba965edb
cleanbrain.developer <cleanbrain.developer@gmail.com>
2024-03-10 21:05:34 +0900

    주석 추가

--- mysql/init.sql
+++ mysql/init.sql
@@ -5,4 +5,5 @@ create table user(
 
 insert into user(name, age) values('cleanbrain', '32');
 
+--mysql8부터 기본 비밀번호 방식이 해싱값으로 변경되어 보정 설정 적용
 alter user 'cleanbrain' identified by with mysql_native_password BY 'cleanbrain'
